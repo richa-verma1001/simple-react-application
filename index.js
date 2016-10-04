73073dc68cf81d131eb31572c60bee0ff40e77a4
Richa Verma <bansalr@us.ibm.com>
2016-10-04 11:03:47 -0500

    Improved Routing Architecture * Added top level application that hold navigation and banner * Child routes should only be updated in content layout section of the page

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Router, Route, browserHistory, IndexRoute } from 'react-router'
-//import App from './modules/App'
+import App from './modules/App'
 import Navigation from './modules/Navigation'
 import About from './modules/About'
 import Repos from './modules/Repos'
@@ -10,7 +10,7 @@ import Home from './modules/Home'
 //render(<App/>, document.getElementById('app'))
 render((
   <Router history={browserHistory}>
-    <Route path="/" component={Navigation}>
+    <Route path="/" component={App}>
       <IndexRoute component={Home}/>
       <Route path="/about" component={About}/>
       <Route path="/repos" component={Repos}/>
