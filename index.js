8851b647f74f86e0b3e64d9cce0f21a58b48d663
Richa Verma <bansalr@us.ibm.com>
2016-10-05 11:21:29 -0500

    Abstract out a routes config object

--- index.js
+++ index.js
@@ -1,25 +1,10 @@
 import 'bootstrap'
 import React from 'react'
 import { render } from 'react-dom'
-import { Router, Route, browserHistory, IndexRoute } from 'react-router'
-import App from './modules/App'
-import Navigation from './modules/Navigation'
-import About from './modules/About'
-import Repos from './modules/Repos'
-import Home from './modules/Home'
-import TBD from './modules/TBD'
-import UserInfo from './modules/UserInfo'
+import { Router, browserHistory } from 'react-router'
+import routes from './config/routes'
 
 //render(<App/>, document.getElementById('app'))
-render((
-  <Router history={browserHistory}>
-    <Route path="/tbd" component={TBD}/>
-    <Route path="/" component={App}>
-      <IndexRoute component={Home}/>
-      <Route path="/about" component={About}/>
-      <Route path="/repos" component={Repos}>
-        <Route path="/repos/:username/:reponame" component={UserInfo}/>
-      </Route>
-    </Route>
-  </Router>
-), document.getElementById('app'))
+render(
+  <Router routes={routes} history={browserHistory}/>,
+  document.getElementById('app'))
